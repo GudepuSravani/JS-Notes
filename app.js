e42f0273b41464c5ec1deae9f9896075e23aa758
[sravani] <sravanigudepu2@gmail.com>
2026-01-26 14:51:08 +0530

    scripting

---
+++ app.js
@@ -0,0 +1,85 @@
+// ctrl + k  + c // to comment
+//console.log("good morning");
+// //a = 2;
+// //b = 4;
+// console.log(a + b);
+
+/////////////////// /*DATA TYPES*/
+// //DIFFERENT TYPES HOLDED STRING,NUM,FLOAT,BOOLEAN
+// var sre = "mumma";
+// var int = 234;
+// console.log(sre,int);
+
+//var a="javascript";
+//var a="javascript";
+//let b=55;
+//console.log(a);
+//console.log(b);
+
+
+  //////////////////*IDENTIFIES:*/
+  //var sravani="hello";
+  //var _name = "hee";
+  //var $age = 23;
+  //var Sravani="hello";
+  //console.log(sravani, _name,$age,Sravani);
+
+/////////////////VARIABLES 4 types
+//it is js are containers that hold reusable Data.
+// var, let, const, nothing.
+//const values are no changed the values
+//var,let are changed
+//var a=233;
+//a++;
+//console.log(a);
+//
+//let ab=233;
+//ab++;
+//console.log(ab);
+//
+//const bc=233;
+//console.log(bc);
+
+/////////////*TYPEOF*/
+ //var a=12;
+ //var b="mumma";
+ //console.log(typeof (a));
+ //console.log(typeof (b));
+ //console.log(a+b)
+
+///////////////////OPERATIONS(5 types)*/ //A symbol represents an action or process.
+ //*Arithmetic operators: +,_,*,%
+//*ASsignment operator : (=)x=y, (+=)x+=y or x=x+y same. -=, *=, /=, %=, **=
+//let a=4;
+//var b=2;
+//b+=5; (or) b=b+5;
+//console.log(a);
+//console.log(b);)
+//*Logistic: And(&&),or(||),not (!),
+// comparsion:==,====,!=,!==,<,>,<=,>=
+//conditional Opeartors: 
+//a>b ? console.log("true"):console.log(false);
+
+/////////////TYPE CONVERSION:[2TYPES] process of converting data one typr to another type/////////
+//(1) Implicit conversion - [automatic type conversion] : 
+//let result;//string numbers only add only strings
+//result = '3' + 2; //or ='4' - '2';
+//console.log(result);
+//(2) Explicit conversion-manual type conversion : 
+//var results ="564";
+//console.log(results);
+////results = Number(results);
+//console.log( typeof(results));
+
+//var a=12.65556;
+//console.log(parseInt(a));
+//console.log(parseFloat(a));
+
+
+//////////////STRING://///////
+//length : To find the length of a string ,use the bulid-in length property.
+// charAt(): indexOf(): toUpperCase(): toLowerCase():, toString():
+//a = "hii it's is js file";
+//ab = 'hii "this" is js file';
+//console.log(ab);
+
